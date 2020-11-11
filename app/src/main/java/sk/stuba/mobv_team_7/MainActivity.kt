95922c666cacb3907b6089e250ba036523bdb606
bh <bhvozda@gmail.com>
2020-11-11 14:39:31 +0100

    init commit

---
+++ app/src/main/java/sk/stuba/mobv_team_7/MainActivity.kt
@@ -0,0 +1,11 @@
+package sk.stuba.mobv_team_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
