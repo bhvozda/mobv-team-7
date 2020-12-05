405c08e1e90dd206278a29f0fee46e9f3a93e732
xhulinova <pata.hulinova@gmail.com>
2020-12-05 16:17:00 +0100

    profile update

--- app/src/main/java/sk/stuba/mobv_team_7/login/LoginFragment.kt
+++ app/src/main/java/sk/stuba/mobv_team_7/login/LoginFragment.kt
@@ -10,12 +10,23 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 import sk.stuba.mobv_team_7.R
+import sk.stuba.mobv_team_7.constants.API_KEY
+import sk.stuba.mobv_team_7.constants.URL
+import sk.stuba.mobv_team_7.data.User
 import sk.stuba.mobv_team_7.databinding.LoginFragmentBinding
+import sk.stuba.mobv_team_7.home.HomeFragmentDirections
+import sk.stuba.mobv_team_7.shared.SharedViewModel
 
 class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var sharedViewModel: SharedViewModel
+    private lateinit var user: User
 
     private lateinit var binding: LoginFragmentBinding
 
@@ -33,33 +44,75 @@ class LoginFragment : Fragment() {
 
         //TODO - to send arguments to other fragments use ViewModelFactory
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
 
         binding.loginViewModel = viewModel
         binding.lifecycleOwner = this
 
+        user = User()
+        binding.user = user
+
         viewModel.eventLoginFinish.observe(viewLifecycleOwner, Observer { loginFinished ->
-            if (loginFinished){
-                loginSuccesful()
+            if (loginFinished) {
+
+                val queue = Volley.newRequestQueue(this.context)
+
+                val username = user.username
+                val password = user.password
+
+                val jsonRoot = JSONObject()
+                jsonRoot.put("action", "login")
+                jsonRoot.put("apikey", API_KEY)
+                jsonRoot.put("username", username)
+                jsonRoot.put("password", password)
+
+                if (!isFormWithErrors(username, password)) {
+                    val jsonRequest = JsonObjectRequest(
+                        URL, jsonRoot,
+                        Response.Listener { response ->
+                            user.email = response.get("email").toString()
+                            user.refreshToken = response.get("refresh").toString()
+                            user.token = response.get("token").toString()
+                            loginSuccessful()
+                        },
+                        Response.ErrorListener {
+                            // TODO: crashanlytics
+                            Toast.makeText(activity, "Login not succesful.", Toast.LENGTH_LONG)
+                                .show()
+                        })
+                    queue.add(jsonRequest)
+                }
+
                 viewModel.onLoginComplete()
             }
         })
 
-        viewModel.eventRegisterFinish.observe(viewLifecycleOwner, Observer { registerFinished ->
-            if (registerFinished){
-                registerSuccesful()
-                viewModel.onRegisterComplete()
-            }
-        })
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegistrationFragment())
+        }
 
         return binding.root
     }
 
-    private fun loginSuccesful() {
+    private fun loginSuccessful() {
+        sharedViewModel.onLoginSuccessful(user)
         findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
     }
 
-    private fun registerSuccesful() {
-        //TODO do something after register
-        Toast.makeText(activity, "Registration done", Toast.LENGTH_LONG).show()
+    private fun isFormWithErrors(username: String, password: String): Boolean {
+        binding.editTextUsername.error = null
+        binding.editTextPassword.error = null
+        if (username.isNullOrEmpty() && password.isNullOrEmpty()) {
+            binding.editTextUsername.error = resources.getString(R.string.empty_error)
+            binding.editTextPassword.error = resources.getString(R.string.empty_error)
+        } else if (username.isNullOrEmpty()) {
+            binding.editTextUsername.error = resources.getString(R.string.empty_error)
+        } else if (password.isNullOrEmpty()) {
+            binding.editTextPassword.error = resources.getString(R.string.empty_error)
+        } else {
+            return false
+        }
+        return true
     }
+
 }
--- app/src/main/java/sk/stuba/mobv_team_7/login/LoginViewModel.kt
+++ app/src/main/java/sk/stuba/mobv_team_7/login/LoginViewModel.kt
@@ -4,35 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LoginViewModel() : ViewModel() {
 
     // The login finished event
     private val _eventLoginFinish = MutableLiveData<Boolean>()
     val eventLoginFinish: LiveData<Boolean>
         get() = _eventLoginFinish
 
-    // The register finished event
-    private val _eventRegisterFinish = MutableLiveData<Boolean>()
-    val eventRegisterFinish: LiveData<Boolean>
-        get() = _eventRegisterFinish
-
     /** Methods for buttons presses **/
     fun onLogin() {
-        //TODO Do the login
         _eventLoginFinish.value = true
-    }
-
-    fun onRegister() {
-        //TODO Do the registration
-        _eventRegisterFinish.value = true
     }
 
     fun onLoginComplete() {
         _eventLoginFinish.value = false
     }
 
-    fun onRegisterComplete() {
-        _eventRegisterFinish.value = false
-    }
 }
