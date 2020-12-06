816a7cdf99c881c78aa33928ebb0989705c98bf7
xhulinova <pata.hulinova@gmail.com>
2020-12-06 16:53:45 +0100

    password

--- app/src/main/java/sk/stuba/mobv_team_7/password/PasswordFragment.kt
+++ app/src/main/java/sk/stuba/mobv_team_7/password/PasswordFragment.kt
@@ -35,23 +35,59 @@ class PasswordFragment : Fragment() {
             container,
             false
         )
-        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
-        sharedViewModel.eventLoginSuccessful.observe(viewLifecycleOwner, Observer { user ->
-            // TODO: password logic
-            if (isCorrectForm(user.password)){
-                println(user.password)}
-
-        })
         binding.buttonChange.setOnClickListener{
-            val queue = Volley.newRequestQueue(this.context)
+            sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+            sharedViewModel.eventLoginSuccessful.observe(viewLifecycleOwner, Observer { user ->
+                // TODO: password logic
+                if (!isCorrectForm(user.password)){
+                    val jsonObject = JSONObject()
+                    jsonObject.put("action", "password")
+                    jsonObject.put("apikey", API_KEY)
+                    jsonObject.put("token", user.token)
+                    jsonObject.put("oldpassword", user.password)
+                    jsonObject.put("newpassword", binding.editTextNewPassword.text.toString())
+
+                    val queue = Volley.newRequestQueue(this.context)
+                    val jsonRequest = JsonObjectRequest(
+                        URL, jsonObject,
+                        Response.Listener { response ->
+                             user.refreshToken = response.get("refresh").toString()
+                             user.token = response.get("token").toString()
+                            changeSuccessful()
+                        },
+                        Response.ErrorListener {
+                            // TODO: crashanlytics
+                            Toast.makeText(activity, "Unexpected error occurred.", Toast.LENGTH_LONG).show()
+                        })
+                    queue.add(jsonRequest)
+                    changeSuccessful()
+                }
+
+            })
+
         }
-        setHasOptionsMenu(true)
         return binding.root
 
     }
     private fun isCorrectForm(password: String):Boolean {
+        binding.editTextOldPass.error = null
+        binding.editTextNewPassword.error = null
+
         if(!binding.editTextOldPass.text.toString().equals(password) && !binding.editTextOldPass.text.isNullOrEmpty()){
             binding.editTextOldPass.error = "Incorrect password"
+            if(binding.editTextNewPassword.text.isNullOrEmpty()){
+                binding.editTextNewPassword.error = resources.getString(R.string.empty_error)
+            }
+        }
+        else if(binding.editTextOldPass.text.isNullOrEmpty() && binding.editTextNewPassword.text.isNullOrEmpty()){
+            binding.editTextOldPass.error = resources.getString(R.string.empty_error)
+            binding.editTextNewPassword.error = resources.getString(R.string.empty_error) }
+        else if (binding.editTextOldPass.text.isNullOrEmpty()) {
+            binding.editTextOldPass.error = resources.getString(R.string.empty_error) }
+        else if (binding.editTextNewPassword.text.isNullOrEmpty()) {
+            binding.editTextNewPassword.error = resources.getString(R.string.empty_error) }
+        else {
+            return false
         }
         return true
     }
@@ -61,26 +97,6 @@ class PasswordFragment : Fragment() {
         findNavController().navigate(PasswordFragmentDirections.actionPasswordFragmentToProfileFragment())
     }
 
-    private fun changePassword(token: String, oldpassword: String, newpassword: String) {
-        val jsonObject = JSONObject()
-        jsonObject.put("action", "password")
-        jsonObject.put("apikey", API_KEY)
-        jsonObject.put("token", token)
-        jsonObject.put("oldpassword", oldpassword)
-        jsonObject.put("newpassword", newpassword)
-
-        val queue = Volley.newRequestQueue(this.context)
-        val jsonRequest = JsonObjectRequest(
-            URL, jsonObject,
-            Response.Listener { response ->
-                changeSuccessful()
-            },
-            Response.ErrorListener {
-                // TODO: crashanlytics
-                Toast.makeText(activity, "Unexpected error occurred.", Toast.LENGTH_LONG).show()
-            })
-        queue.add(jsonRequest)
-    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(PasswordViewModel::class.java)
