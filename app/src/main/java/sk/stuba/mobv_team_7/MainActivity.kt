91c1253740649474cad42d3195e14207a27f5715
xhulinova <pata.hulinova@gmail.com>
2020-12-06 20:29:20 +0100

    profile

--- app/src/main/java/sk/stuba/mobv_team_7/MainActivity.kt
+++ app/src/main/java/sk/stuba/mobv_team_7/MainActivity.kt
@@ -2,6 +2,8 @@ package sk.stuba.mobv_team_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
